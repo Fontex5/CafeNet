cd3f4166b61760aa28ad87b86f7cd31e1276ed88
Mohammad Moradi <53098070+Fontex5@users.noreply.github.com>
2020-09-30 21:45:35 +0330

    Update main.cpp

--- CafeNet/main.cpp
+++ CafeNet/main.cpp
@@ -55,17 +55,24 @@ int main()
 }
 
 void Initialize() {
-
+    string username;
+    
     Date bd(20, 4, 1994);
     Account a1("Ali", "098884142", bd);
+    username = "Ali";
+    a1.setUsername(username);
     AccList.push_back(a1);
 
     bd.setDate(2, 8, 2000);
     Account a2("Sadegh", "08644843", bd);
+    username = "Sadegh";
+    a2.setUsername(username);
     AccList.push_back(a2);
 
     bd.setDate(5, 9, 1990);
     Account a3("Saeed", "094483133", bd);
+    username = "Saeed";
+    a3.setUsername(username);
     AccList.push_back(a3);
 
     double price = 15000;
@@ -171,10 +178,12 @@ void Signin_Account() {
                 cin >> temp;
             }
             if (temp == item.getPassword()) { Show_Account_Menu(item); }
-            else { break; }
+            else { 
+                cout << "Account not Found."<<endl;
+                break;
+            }
         }
     }
-    cout << "Account not Found."<<endl;
 }
 
 void Show_Account_Menu(Account& a) {
