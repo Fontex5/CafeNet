0b7e5cc9f4521e86b0578f9b4ac05e5ceebad69e
Mohammad Moradi <53098070+Fontex5@users.noreply.github.com>
2020-09-30 23:29:26 +0330

    Update Manager.cpp

--- CafeNet/src/Manager.cpp
+++ CafeNet/src/Manager.cpp
@@ -14,11 +14,11 @@ Manager::Manager(string n , string p , Date d) : Human(n , p , d)
 void Manager::Add_Computer(vector<Computer> &ComList){
     string id , cpu ,ram; // temp strings to read data from user
     cout<<"Enter computer ID: ";
-    cin>>id;
+    getline(cin, id);
     cout<<"Enter Computer's CPU: ";
-    cin>>cpu;
+    getline(cin, cpu);
     cout<<"Enter Computer's RAM: ";
-    cin>>ram;
+    getline(cin, ram);
 
     Computer c(id,cpu,ram);
 
@@ -32,9 +32,9 @@ void Manager::Add_Computer(vector<Computer> &ComList){
 }
 void Manager::Delete_Computer(vector<Computer>& ComList) {
     int i = 0;
-    cout << setw(7) << "ID" << setw(5) << "CPU" << setw(5) << "RAM" << setw(5) << "Price/Hour" << endl;
+    cout << setw(6) << "ID" << setw(10) << "CPU" << setw(10) << "RAM" << setw(10) << "Price/Hour" << endl;
     for (auto item : ComList) {
-        cout << i << "." << setw(5) << item.id << setw(5) << item.cpu << setw(5) << item.ram << setw(5) << item.getPrice() << endl;
+        cout << i << "." << setw(10) << item.id << setw(10) << item.cpu << setw(10) << item.ram << setw(10) << item.getPrice() << endl;
         i++;
     }
     cout << "Which Computer you want to delete ? (Enter row number) ";
@@ -46,7 +46,7 @@ void Manager::Add_Account(vector<Account>& AccList) {
     string name, phone;
     unsigned int year, month, day;
     cout << "Enter User's Name: ";
-    cin >> name;
+    getline(cin, name);
     cout << "Enter Your Phone number: ";
     cin >> phone;
     cout << "Enter your birth day inorder , Year: ";
