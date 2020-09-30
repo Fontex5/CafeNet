06ff798a65b8cb9466cfb3efcb880ef16455c498
Mohammad Moradi <53098070+Fontex5@users.noreply.github.com>
2020-09-30 21:29:58 +0330

    Update Computer.cpp

--- CafeNet/src/Computer.cpp
+++ CafeNet/src/Computer.cpp
@@ -6,9 +6,16 @@ using namespace std;
 Computer::Computer(const string i, const string c , const string r): id(i) , cpu(c) , ram(r){
     startT.setTime(0, 0);
     finishT.setTime(0,0);
+    double d = 0;
+    setPrice(d);
 }
 
-Computer::Computer(){}
+Computer::Computer(){
+    startT.setTime(0, 0);
+    finishT.setTime(0, 0);
+    double d = 0;
+    setPrice(d);
+}
 
 void Computer::setPrice(double &p){
     while(p>15000){
@@ -29,6 +36,10 @@ void Computer::setFinishTime(const Time &t){ finishT = t;}
 
 Time Computer::getFinishTime() const { return finishT;}
 
-
-
-
+void Computer::Show_Computer() const {
+    string reserved="Yes";
+    Time t;
+    t.setTime(0, 0);
+    if (this->getStartTime() == t) { reserved = "Not"; }
+    cout << id << setw(10) << cpu << setw(10) << ram << setw(10) << price << setw(10) << reserved << endl;
+}
